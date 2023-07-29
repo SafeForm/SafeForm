c9fd1faabefc1b59c53e4f640399e971f903818b
SafeForm <112861660+SafeForm@users.noreply.github.com>
2023-07-29 20:19:04 +1000

    Update userPrograms.js

--- userPrograms.js
+++ userPrograms.js
@@ -15,11 +15,11 @@ function mainFunc() {
   }
 
     //Loop through all list items and assign href to each workout
-    const programWorkoutList = document.getElementById("workoutList").children;
+    const programWorkoutList = document.getElementById("programWorkoutList").children;
 
     for (var i = 0; i < programWorkoutList.length; i++) {
       //Get link from workout summary information and set thumbnail link
-      var workoutSummaryLink = programWorkoutList[i].querySelector("#workoutSummaryLink").href;
+      var workoutSummaryLink = programWorkoutList[i].querySelector("#svgPersonLink").href;
       programWorkoutList[i].querySelector("#thumbnailLink").href = workoutSummaryLink;
     }
 //
