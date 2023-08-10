d4398011d6ad8217a4940a94f0ca12c83d797c0e
SafeForm <112861660+SafeForm@users.noreply.github.com>
2023-08-10 20:52:38 +1000

    Update workoutSummary.js

--- workoutSummary.js
+++ workoutSummary.js
@@ -1,55 +1,129 @@
-const devModeFlag = localStorage.getItem("devMode");
 if (document.readyState !== 'loading') {
-  if(devModeFlag != null) {
-    mainFuncLocal();
-  }
+  main();
 } else {
   document.addEventListener('DOMContentLoaded', function () {
-  if(devModeFlag != null) {
-    mainFuncLocal();
-  }
+      main();
   });
 }
 
-function mainFuncLocal() {
-
-  document.getElementById("summarySwitch").style.display = "block";
+async function main() {
 
   //Add inputs for each exercise based on number of sets
   const inputList = document.getElementById("inputList").children;
 
-  for(var i = 0; i < inputList.length; i++) {
+  MemberStack.onReady.then(async function(member) {  
 
-    //Get number of sets for that exercise
-    const numberOfSets = parseInt(inputList[i].querySelector("#setsInput").innerText);
+    var memberJSON = await member.getMetaData();
 
-    //Get rest info for that exercise
-    const restDiv = inputList[i].querySelector("#inputRest");
-    var newRestDiv = restDiv.cloneNode(true);
-    newRestDiv.style.display = "flex";
-    //Get input section for cloning
-    const inputSectionPlaceholder = inputList[i].querySelector("#inputSection");
-    var exerciseInputSection = inputList[i].querySelector("#inputSectionBlock");
-    exerciseInputSection.appendChild(newRestDiv);
+    for(var i = 0; i < inputList.length; i++) {
 
-    for(var j = 0; j < numberOfSets-1; j++) {
-
-      newRestDiv = restDiv.cloneNode(true);
-
-      var newInputSection = inputSectionPlaceholder.cloneNode(true);
+      //Get number of sets for that exercise
+      const numberOfSets = parseInt(inputList[i].querySelector("#setsInput").innerText);
+  
+      //Get rest info for that exercise
+      const restDiv = inputList[i].querySelector("#inputRest");
+      var newRestDiv = restDiv.cloneNode(true);
       newRestDiv.style.display = "flex";
+      //Get input section for cloning
+      const inputSectionPlaceholder = inputList[i].querySelector("#inputSection");
+      var exerciseInputSection = inputList[i].querySelector("#inputSectionBlock");
+      const exerciseName = exerciseInputSection.querySelector("#exerciseNameInput").innerText;
+      var weightInput = exerciseInputSection.querySelector("#weight");
+
+      var repsInput = exerciseInputSection.querySelector("#reps");
+
+      weightInput.addEventListener('blur', function(event) {
+        const inputValue = event.target.value;
+  
+        const exerciseBlock = event.target.closest("#inputSectionBlock");
+        const exerciseName = exerciseBlock.querySelector("#exerciseNameInput");
+  
+        updateExerciseDetails(exerciseName.innerText, inputValue, null, "weight");
+      });
+
+      repsInput.addEventListener('blur', function(event) {
+        const inputValue = event.target.value;
+  
+        const exerciseBlock = event.target.closest("#inputSectionBlock");
+        const exerciseName = exerciseBlock.querySelector("#exerciseNameInput");
+  
+        updateExerciseDetails(exerciseName.innerText, inputValue, 0, "reps");
+      });
+
+      var memberJSONExerciseName = memberJSON[exerciseName];
       
-      
-      exerciseInputSection.appendChild(newInputSection);
-      if(j < numberOfSets - 2) {
-        exerciseInputSection.appendChild(newRestDiv);
+      if(memberJSONExerciseName != undefined) {
+
+        if(memberJSONExerciseName.weight != undefined) {
+          weightInput.value = memberJSONExerciseName.weight;
+        }
+
+        if(memberJSONExerciseName.reps != undefined) {
+          if(memberJSONExerciseName.reps.length > 0) {
+            repsInput.value = memberJSONExerciseName.reps[0];
+          }
+          
+        }
+        
       }
       
+      exerciseInputSection.appendChild(newRestDiv);
+
+      for (var j = 0; j < numberOfSets - 1; j++) {
+        (function(j) {
+          newRestDiv = restDiv.cloneNode(true);
+  
+          var newInputSection = inputSectionPlaceholder.cloneNode(true);
+          newRestDiv.style.display = "flex";
+  
+          var newWeightInput = newInputSection.querySelector("#weight");
+          var newRepsInput = newInputSection.querySelector("#reps");
+          console.log(newRepsInput);
+  
+          newWeightInput.addEventListener('blur', function(event) {
+            const inputValue = event.target.value;
+      
+            const exerciseBlock = event.target.closest("#inputSectionBlock");
+            const exerciseName = exerciseBlock.querySelector("#exerciseNameInput");
+      
+            updateExerciseDetails(exerciseName.innerText, inputValue, null, "weight");
+          });
+  
+          newRepsInput.addEventListener('blur', function(event) {
+            const inputValue = event.target.value;
+      
+            const exerciseBlock = event.target.closest("#inputSectionBlock");
+            const exerciseName = exerciseBlock.querySelector("#exerciseNameInput");
+      
+            updateExerciseDetails(exerciseName.innerText, inputValue, j+1, "reps");
+          });
+          newRepsInput.value = "";
+          
+          if(memberJSONExerciseName != null) {
+
+            if(memberJSONExerciseName.weight != "undefined" && memberJSONExerciseName.weight != null) {
+              newWeightInput.value = memberJSONExerciseName.weight;
+            }
+            
+
+            if(memberJSONExerciseName.reps != undefined) {
+              if(memberJSONExerciseName.reps.length > j+1) {
+                newRepsInput.value = memberJSONExerciseName.reps[j+1];
+              } 
+            }
+          }
+          
+          exerciseInputSection.appendChild(newInputSection);
+          if(j < numberOfSets - 2) {
+            exerciseInputSection.appendChild(newRestDiv);
+          }
+        })(j); // Pass the value of j into the immediately-invoked function expression (IIFE)
+      }
+  
+  
     }
 
-
-  }
-
+  });
 
   //Hide the first exercise breaker of the exercise list
   const exerciseList = document.getElementById("listOfExercises").children;
@@ -143,4 +217,53 @@ function mainFuncLocal() {
       console.log(err);
     }
   }
+
+  async function updateExerciseDetails(exerciseName, inputValue, setNumber=null, type) {
+    MemberStack.onReady.then(async function(member) {  
+
+      var metadata = await member.getMetaData();
+
+      //Get info from exercise
+      var exerciseInfo = metadata[exerciseName];
+      console.log(exerciseInfo);
+
+      if(exerciseInfo == undefined) {
+        exerciseInfo = {"weight": "", "reps":[]};
+      }
+ 
+      if(type == "reps") {
+        //If no rep info recorded
+        var repsArr = exerciseInfo.reps;
+
+        if(repsArr == null) {
+          //Reps array is empty so just put input value into array and update
+          var exerciseRepsObj = [inputValue];
+        } else {
+          //There is rep info
+          //Check if index in reps array exists first
+          if(setNumber < repsArr.length) {
+            repsArr[setNumber] = inputValue;
+          } else {
+            repsArr.push(inputValue)
+          }
+
+          var exerciseRepsObj = repsArr;
+          
+        }
+        exerciseInfo.reps = exerciseRepsObj;
+        
+        const updatedJSON = {[exerciseName] : exerciseInfo};
+
+        member.updateMetaData(updatedJSON);
+
+      } else {
+        exerciseInfo.weight = inputValue;
+        const updatedJSON = {[exerciseName] : exerciseInfo};
+
+        member.updateMetaData(updatedJSON);
+      }
+
+    });
+  }
+
 }
