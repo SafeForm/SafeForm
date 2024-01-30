466d3f619cd882dc80cc4e377ef5ba34a80e75de
SafeForm <112861660+SafeForm@users.noreply.github.com>
2024-01-30 22:24:24 +0100

    Create post-sign-up.js

---
+++ post-sign-up.js
@@ -0,0 +1,44 @@
+if (document.readyState !== 'loading') {
+  mainFunc();
+} else {
+  document.addEventListener('DOMContentLoaded', function () {
+    mainFunc();
+  });
+}
+
+function mainFunc() {
+  MemberStack.onReady.then(async function (member) {
+    // Function to check member page and redirect if not set
+    async function checkMemberPage() {
+      var membershipId = member.id;
+
+      try {
+        const response = await fetch("https://hook.us1.make.com/gyuuh5tiw2ey28dqrt9xpqjq633l925h", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            memberID: membershipId,
+          }),
+        });
+
+        const responseData = await response.text();
+
+        // If the response is not empty, redirect the user to the page
+        if (responseData.trim().toLowerCase() != 'accepted') {
+          window.location.href = window.location.origin + `/user-programs/${responseData}`;
+        } else {
+          console.log("Empty member page");
+        }
+      } catch (error) {
+        // Handle any errors that occurred during the fetch
+        console.error('Error checking member page:', error);
+      }
+    }
+
+    // Check for member page every 7 seconds
+    const intervalId = setInterval(checkMemberPage, 7000);
+
+  });
+}
