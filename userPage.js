7b6940d624d3f9f6f94419c728b71633b1b4d79c
SafeForm <112861660+SafeForm@users.noreply.github.com>
2024-06-16 18:16:08 +1000

    Create userPage.js

---
+++ userPage.js
@@ -0,0 +1,100 @@
+const devMode = localStorage.getItem("devMode");
+if (document.readyState !== 'loading') {
+  if(devMode == undefined) {
+    main();
+  }
+  
+} else {
+  document.addEventListener('DOMContentLoaded', function () {
+    if(devMode == undefined) {
+      main();
+    }
+  });
+}
+
+function main() {
+
+  //Show all status
+  var workoutStatus = document.querySelectorAll("#workoutStatus");
+  for(var i = 0; i < workoutStatus.length; i++) {
+    workoutStatus[i].style.display = "block";
+  }
+
+  MemberStack.onReady.then(async function(member) {  
+
+    var metadata = await member.getMetaData();
+
+    // Get the programs list
+    const programsList = document.getElementById("programsList").children;
+    const programsArray = Array.from(programsList); // Create a separate array of elements
+
+    for (var i = 0; i < programsArray.length; i++) {
+      var programID = programsArray[i].querySelector(".programid").innerText;
+
+      if (metadata["currentProgram"] == programID) {
+        programsArray[i].querySelector("#workoutStatus").classList.remove("not-started");
+        programsArray[i].querySelector("#workoutStatus").classList.add("in-progress");
+        
+        // Move to in progress div
+        document.querySelector("#inProgressDiv").appendChild(programsArray[i]);
+      }
+      
+      if (metadata[programID] && programsArray[i].querySelector(".completed").innerText.toLowerCase() != "true") {
+        var totalNumberOfWeeks = programsArray[i].querySelector("#workoutStatus").innerText;
+        programsArray[i].querySelector("#workoutStatus").innerText = `${metadata[programID]} / ${totalNumberOfWeeks} weeks`;
+      } else {
+        var totalNumberOfWeeks = programsArray[i].querySelector("#workoutStatus").innerText;
+        programsArray[i].querySelector("#workoutStatus").innerText = `${totalNumberOfWeeks} weeks`;
+      }
+
+      if (programsArray[i].querySelector(".completed").innerText.toLowerCase() == "true") {
+        programsArray[i].querySelector("#workoutStatus").classList.remove("not-started");
+        programsArray[i].querySelector("#workoutStatus").classList.add("finished");
+        
+        // Move to done div
+        document.querySelector("#doneDiv").appendChild(programsArray[i]);
+      }
+
+    }
+
+    const challengeList = document.getElementById("challengesList").children;
+    const challengesArray = Array.from(challengeList); // Create a separate array of elements
+    const currentDate = new Date(); // Get the current date
+
+    for (var i = 0; i < challengesArray.length; i++) {
+      const challengeEndDateText = challengesArray[i].querySelector(".completed").innerText; // in the form 2024-05-31
+      const challengeEndDate = new Date(challengeEndDateText); // Convert to Date object
+
+      const challengeStatusElement = challengesArray[i].querySelector("#workoutStatus");
+      
+      if (currentDate <= challengeEndDate) {
+        // Challenge is in progress
+        challengeStatusElement.classList.remove("not-started");
+        challengeStatusElement.classList.add("in-progress");
+        challengeStatusElement.innerText = "In Progress";
+        
+        // Move to in progress div
+        document.querySelector("#inProgressDiv").appendChild(challengesArray[i]);
+      } else {
+        // Challenge is finished
+        challengeStatusElement.classList.remove("not-started");
+        challengeStatusElement.classList.add("finished");
+        challengeStatusElement.innerText = "Finished";
+        
+        // Move to done div
+        document.querySelector("#doneDiv").appendChild(challengesArray[i]);
+      }
+    }
+
+    var headerDivs = document.querySelectorAll(".program-type-header");
+    for(var i = 0; i < headerDivs.length; i++) {
+      headerDivs[i].style.display = "block";
+    }
+
+
+
+
+
+  });
+
+}
